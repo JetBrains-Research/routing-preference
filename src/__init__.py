44a5814c5ca1da27c11fa0487e8a4b6613548a2b
iliall <ialenabi@uwaterloo.ca>
2026-03-16 12:33:17 +0100

    Removed Collecter

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,16 @@
 """Routing Preference."""
 
-from .models import Issue, Solution
-from .collector import IssueCollector
+from .dataset import IssueDataset
 from .generator import SolutionGenerator
-from .storage import SolutionStorage
+from .models import Issue, Solution
 from .pipeline import Pipeline
+from .storage import SolutionStorage
 
 __all__ = [
     "Issue",
+    "IssueDataset",
+    "Pipeline",
     "Solution",
-    "IssueCollector",
     "SolutionGenerator",
     "SolutionStorage",
-    "Pipeline",
 ]
